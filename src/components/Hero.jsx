9b64bc82b4f4f96a3af2aa2272890d13fabf35bd
Adarsh Ranjan Nayak <adarshranjan2003@gmail.com>
2024-11-23 11:12:49 +0530

    updated some files that I worked on

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import { useState } from 'react';
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
+import { useGSAP } from '@gsap/react';
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -23,6 +24,8 @@ const Hero = () => {
 
         setCurrentIndex(upcomingVideoIndex);
     }
+
+    useGSAP
 
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
   return (
@@ -55,7 +58,7 @@ const Hero = () => {
 
                 <video 
                     src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-                    //autoPlay
+                    autoPlay
                     loop
                     muted
                     className="absolute left-0 top-0 size-full object-cover object-center"
@@ -73,10 +76,14 @@ const Hero = () => {
 
                     <p className="mb-5 max-w-64  font-robert-regular text-blue-100">Enter the Metagame Layer <br /> Unleash the Play Economy</p>
 
-                    <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="bg-yellow-300 flex-center gap-1" />
+                    <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow />} containerClass="!bg-yellow-300 flex-center gap-1" />
                 </div>
             </div>
         </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+                G<b>a</b>ming
+            </h1>
     </div>
   )
 }
